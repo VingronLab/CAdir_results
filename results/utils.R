2a41559a2f92c0b529cb2df5c6fecc6e0b0e6da8
Clemens Kohl <kohl.clemens@gmail.com>
2024-08-12 14:49:58 +0200

    updated figure2 with new angle

--- results/utils.R
+++ results/utils.R
@@ -145,11 +145,47 @@ plot_clusters_custom <- function(cadir,
                                  label_genes = FALSE,
                                  ntop = 5,
                                  text_size = 16,
-                                 outlier_cluster) {
+                                 ggncol = NULL,
+                                 ggnrow = NULL,
+                                 outlier_cluster,
+                                 axis = FALSE,
+                                 title_prefix = "_",
+                                 gsub_title = NULL,
+                                 legend_pos = "none",
+                                 return_list = FALSE) {
   pls <- list()
   cls <- levels(cadir@cell_clusters)
 
+  if (isFALSE(axis)) {
+    plot_theme <- ggplot2::theme(
+      legend.position = legend_pos,
+      axis.title.x = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank(),
+      axis.ticks.x = ggplot2::element_blank(),
+      axis.title.y = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_blank(),
+      axis.ticks.y = ggplot2::element_blank(),
+      plot.title = ggplot2::element_text(size = text_size)
+    )
+  } else {
+    plot_theme <- ggplot2::theme(
+      legend.position = legend_pos,
+      axis.title.x = ggplot2::element_blank(),
+      axis.title.y = ggplot2::element_blank(),
+      plot.title = ggplot2::element_text(size = text_size)
+    )
+  }
+
   for (i in seq_along(cls)) {
+    if (!is.null(gsub_title)) {
+      cls_title <- gsub(
+        pattern = gsub_title,
+        replacement = " ",
+        x = cls[i]
+      )
+    } else {
+      cls_title <- cls[i]
+    }
     p <- cluster_apl(
       caobj = caobj,
       cadir = cadir,
@@ -166,17 +202,9 @@ plot_clusters_custom <- function(cadir,
       size_factor = size_factor,
       ntop = ntop
     ) +
-      ggplot2::ggtitle(paste0("Cluster: ", cls[i])) +
-      ggplot2::theme(
-        legend.position = "none",
-        axis.title.x = ggplot2::element_blank(),
-        axis.text.x = ggplot2::element_blank(),
-        axis.ticks.x = ggplot2::element_blank(),
-        axis.title.y = ggplot2::element_blank(),
-        axis.text.y = ggplot2::element_blank(),
-        axis.ticks.y = ggplot2::element_blank(),
-        plot.title = ggplot2::element_text(size = text_size)
-      )
+      ggplot2::ggtitle(paste0(title_prefix, cls_title)) +
+      plot_theme
+
 
     if (cls[i] == outlier_cluster) {
       cell_rnk <- get_top_cells(
@@ -190,166 +218,166 @@ plot_clusters_custom <- function(cadir,
           aes(x = x, y = y),
           color = "#d44a3d",
         )
-      # +
-      #   ggrepel::geom_label_repel(
-      #   data = cell_rnk[cell_rnk$Score >= 0, ],
-      #   ggplot2::aes(
-      #     x = x,
-      #     y = y,
-      #     label = Colname
-      #   ),
-      #   color = "#d44a3d",
-      #   box.padding = 1,
-      #   max.overlaps = Inf
-      # )
     }
 
     pls[[i]] <- p
   }
 
-  fig <- ggpubr::ggarrange(
-    plotlist = pls,
-    nrow = ceiling(sqrt(length(cls))),
-    ncol = ceiling(sqrt(length(cls)))
-  )
+  if (isTRUE(return_list)) {
+    fig <- pls
+  } else {
+    fig <- ggpubr::ggarrange(
+      plotlist = pls,
+      nrow = ifelse(test = is.null(ggnrow),
+        yes = ceiling(sqrt(length(cls))),
+        no = ggnrow
+      ),
+      ncol = ifelse(test = is.null(ggncol),
+        yes = ceiling(sqrt(length(cls))),
+        no = ggncol
+      )
+    )
+  }
   return(suppressWarnings(fig))
 }
 
 
 
 sm_plot_custom <- function(cadir,
-                    caobj,
-                    rm_redund = TRUE,
-                    show_cells = TRUE,
-                    show_genes = FALSE,
-                    highlight_cluster = FALSE,
-                    annotate_clusters = FALSE,
-                    org = "mm",
-                    keep_end = TRUE) {
-    # TODO: Simplify function.
-    base::stopifnot(
-        "Set either `show_cells` or `show_genes` to TRUE." =
-            isTRUE(show_cells) || isTRUE(show_genes)
-    )
+                           caobj,
+                           rm_redund = TRUE,
+                           show_cells = TRUE,
+                           show_genes = FALSE,
+                           highlight_cluster = FALSE,
+                           annotate_clusters = FALSE,
+                           org = "mm",
+                           keep_end = TRUE) {
+  # TODO: Simplify function.
+  base::stopifnot(
+    "Set either `show_cells` or `show_genes` to TRUE." =
+      isTRUE(show_cells) || isTRUE(show_genes)
+  )
 
-    graph <- build_graph(
-        cadir = cadir,
-        rm_redund = rm_redund,
-        keep_end = keep_end
-    )
+  graph <- build_graph(
+    cadir = cadir,
+    rm_redund = rm_redund,
+    keep_end = keep_end
+  )
 
-    lgraph <- ggraph::create_layout(graph, layout = "tree")
+  lgraph <- ggraph::create_layout(graph, layout = "tree")
 
-    ggraph::set_graph_style(plot_margin = ggplot2::margin(0, 0, 0, 0))
-    bg <- ggraph::ggraph(lgraph) +
-        ggraph::geom_edge_link() +
-        ggraph::geom_node_point(alpha = 1)
+  ggraph::set_graph_style(plot_margin = ggplot2::margin(0, 0, 0, 0))
+  bg <- ggraph::ggraph(lgraph) +
+    ggraph::geom_edge_link() +
+    ggraph::geom_node_point(alpha = 1)
 
-    bg_coords <- get_x_y_values(bg)
+  bg_coords <- get_x_y_values(bg)
 
-    cls <- cadir@log$clusters
-    dirs <- cadir@log$directions
+  cls <- cadir@log$clusters
+  dirs <- cadir@log$directions
 
-    nodes <- names(igraph::V(graph))
+  nodes <- names(igraph::V(graph))
 
-    old_iter_nm <- ""
-    for (i in seq_len(nrow(lgraph))) {
-        node_nm <- nodes[i]
+  old_iter_nm <- ""
+  for (i in seq_len(nrow(lgraph))) {
+    node_nm <- nodes[i]
 
-        name_elems <- base::strsplit(node_nm, "-", fixed = TRUE)[[1]]
-        # name_elems <- stringr::str_split_1(node_nm, "-")
+    name_elems <- base::strsplit(node_nm, "-", fixed = TRUE)[[1]]
+    # name_elems <- stringr::str_split_1(node_nm, "-")
 
-        if (name_elems[1] == "root") next
+    if (name_elems[1] == "root") next
 
-        iter_nm <- name_elems[1]
-        cluster <- name_elems[2]
+    iter_nm <- name_elems[1]
+    cluster <- name_elems[2]
 
-        grp_idx <- base::which(cls[, iter_nm] == cluster)
+    grp_idx <- base::which(cls[, iter_nm] == cluster)
 
-        is_iter_dirs <- dirs$iter == iter_nm
-        coord_column <- !colnames(dirs) %in% c("iter", "dirname")
+    is_iter_dirs <- dirs$iter == iter_nm
+    coord_column <- !colnames(dirs) %in% c("iter", "dirname")
 
-        tmp_dirs <- dirs[is_iter_dirs, coord_column]
-        rownames(tmp_dirs) <- dirs[is_iter_dirs, "dirname"]
+    tmp_dirs <- dirs[is_iter_dirs, coord_column]
+    rownames(tmp_dirs) <- dirs[is_iter_dirs, "dirname"]
 
-        cluster_idx <- base::which(rownames(tmp_dirs) == cluster)
-        dir <- tmp_dirs[cluster_idx, ]
+    cluster_idx <- base::which(rownames(tmp_dirs) == cluster)
+    dir <- tmp_dirs[cluster_idx, ]
 
-        if (iter_nm != old_iter_nm) {
-            tmp_ccs <- x2f(cls[, iter_nm])
-            names(tmp_ccs) <- rownames(caobj@prin_coords_cols)
+    if (iter_nm != old_iter_nm) {
+      tmp_ccs <- x2f(cls[, iter_nm])
+      names(tmp_ccs) <- rownames(caobj@prin_coords_cols)
 
-            tmp_cadir <- methods::new(
-                "cadir",
-                cell_clusters = tmp_ccs,
-                directions = as.matrix(tmp_dirs)
-            )
+      tmp_cadir <- methods::new(
+        "cadir",
+        cell_clusters = tmp_ccs,
+        directions = as.matrix(tmp_dirs)
+      )
 
-            if (is.null(cadir@parameters$qcutoff)) {
-                cadir@parameters$qcutoff <- 0.8
-            }
+      if (is.null(cadir@parameters$qcutoff)) {
+        cadir@parameters$qcutoff <- 0.8
+      }
 
-            tmp_cadir@gene_clusters <- assign_genes(
-                caobj = caobj,
-                cadir = tmp_cadir,
-                qcutoff = cadir@parameters$qcutoff
-            )
+      tmp_cadir@gene_clusters <- assign_genes(
+        caobj = caobj,
+        cadir = tmp_cadir,
+        qcutoff = cadir@parameters$qcutoff
+      )
 
-            if (isTRUE(annotate_clusters)) {
-                suppressWarnings({
-                    tmp_cadir <- annotate_biclustering(
-                        obj = tmp_cadir,
-                        universe = rownames(caobj@std_coords_rows),
-                        org = org,
-                        alpha = 0.05,
-                        min_size = 10,
-                        max_size = 500
-                    )
-                })
-            }
-            old_iter_nm <- iter_nm
-        }
-
-        cluster <- rownames(tmp_cadir@directions)[cluster_idx]
-        rownames(dir) <- cluster
-
-        # colour_by_group <- !highlight_cluster
-
-        p <- cluster_apl(
-            caobj = caobj,
-            cadir = tmp_cadir,
-            direction = as.numeric(dir),
-            group = grp_idx,
-            cluster = cluster,
-            show_cells = show_cells,
-            show_genes = show_genes,
-            highlight_cluster = highlight_cluster,
-            show_lines = FALSE,
-            point_size = 0.3
-        )
-        if (isTRUE(annotate_clusters)) {
-            p <- p +
-                ggplot2::ggtitle(cluster) +
-                theme_blank(
-                    title = ggplot2::element_text(color = "black",
-                                                  size = 10, face = "bold"),
-                    text = ggplot2::element_text()
-                )
-        } else {
-            #TODO: We need to pick a color palette for a large number of clusters
-            # scale_color_mpimg(name = "mpimg") +
-            p <- p + theme_blank()
-        }
-
-        bg <- bg +
-            patchwork::inset_element(p,
-                left = bg_coords[i, 1] - 0.07,
-                right = bg_coords[i, 1] + 0.07,
-                top = bg_coords[i, 2] + 0.07,
-                bottom = bg_coords[i, 2] - 0.07,
-                align_to = "panel"
-            )
+      if (isTRUE(annotate_clusters)) {
+        suppressWarnings({
+          tmp_cadir <- annotate_biclustering(
+            obj = tmp_cadir,
+            universe = rownames(caobj@std_coords_rows),
+            org = org,
+            alpha = 0.05,
+            min_size = 10,
+            max_size = 500
+          )
+        })
+      }
+      old_iter_nm <- iter_nm
     }
 
-    return(bg)
+    cluster <- rownames(tmp_cadir@directions)[cluster_idx]
+    rownames(dir) <- cluster
+
+    # colour_by_group <- !highlight_cluster
+
+    p <- cluster_apl(
+      caobj = caobj,
+      cadir = tmp_cadir,
+      direction = as.numeric(dir),
+      group = grp_idx,
+      cluster = cluster,
+      show_cells = show_cells,
+      show_genes = show_genes,
+      highlight_cluster = highlight_cluster,
+      show_lines = FALSE,
+      point_size = 0.3
+    )
+    if (isTRUE(annotate_clusters)) {
+      p <- p +
+        ggplot2::ggtitle(cluster) +
+        theme_blank(
+          title = ggplot2::element_text(
+            color = "black",
+            size = 10, face = "bold"
+          ),
+          text = ggplot2::element_text()
+        )
+    } else {
+      # TODO: We need to pick a color palette for a large number of clusters
+      # scale_color_mpimg(name = "mpimg") +
+      p <- p + theme_blank()
+    }
+
+    bg <- bg +
+      patchwork::inset_element(p,
+        left = bg_coords[i, 1] - 0.07,
+        right = bg_coords[i, 1] + 0.07,
+        top = bg_coords[i, 2] + 0.07,
+        bottom = bg_coords[i, 2] - 0.07,
+        align_to = "panel"
+      )
+  }
+
+  return(bg)
 }
